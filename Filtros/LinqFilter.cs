8f3506fd733b6267a8f69df10e468ac7be1b0532
Hiago 7k <hiago7k@gmail.com>
2026-03-18 20:38:46 -0300

    usando linq

---
+++ Filtros/LinqFilter.cs
@@ -0,0 +1,23 @@
+﻿using DesafioApi4.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DesafioApi4.Filtros;
+using DesafioApi4.Modelos;
+
+internal class LinqFilter
+{
+    public static void ListarLivrosFiltrados(List<Livro> livros) 
+    {
+        var livrosFiltrados = livros.Select(livros => livros.Nome).Distinct().ToList();
+        foreach (var filme in livrosFiltrados) 
+        {
+            Console.WriteLine("- {filme}");
+        }
+    }
+
+}
