3cb4ef4800ac3d2f7901e85144e57130726b0d09
Hiago 7k <hiago7k@gmail.com>
2026-03-19 19:31:56 -0300

    iniciando novo desafi

--- Filtros/LinqFilter.cs
+++ Filtros/LinqFilter.cs
@@ -14,7 +14,7 @@ internal class LinqFilter
     public static void ListarLivrosFiltrados(List<Livro> livros) 
     {
         // var livrosFiltrados = livros.Select(livros => livros.Nome).Distinct().ToList();
-        var livrosFiltrados = livros.Select(livros => livros.Nome.OrderBy(livros.Nome))!;
+        var livrosFiltrados = livros.Where(livros => livros.AnoPublicacao < 1950).OrderBy(livros => livros.Nome).Select(livros => livros.Nome).ToList();
         foreach (var filme in livrosFiltrados) 
         {
             Console.WriteLine($"- {filme}");
